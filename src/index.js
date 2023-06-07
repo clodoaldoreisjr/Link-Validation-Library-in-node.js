7b3c70f9322714991464abc8a4cfd589a97a3e5a
Clodoaldo <clodoaldo.junior.crmj@gmail.com>
2023-06-07 15:46:21 -0300

    Project/Challenger Complete!

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados.length !== 0 ? resultados : 'Não há links no arquivo';
+}
+
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não há Arquivo no diretório'));
+}
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const enconding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, enconding)
+        return extraiLinks(texto);
+    } catch (erro) {
+        trataErro(erro)
+    }
+
+}
+
+export default pegaArquivo;
+
+
